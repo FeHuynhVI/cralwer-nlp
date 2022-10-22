d35ff0c7cfd714c9113348cd52a78c0822cbe597
HUYNH TRIEU VI <VIHT7@FPT.EDU.VN>
2022-10-22 21:13:12 +0700

    Commit

--- crawler/spiders/vnexpress.py
+++ crawler/spiders/vnexpress.py
@@ -2,7 +2,8 @@
 vnexpress crawler.
 """
 import scrapy
-import framler     
+import requests
+from bs4 import BeautifulSoup 
 
 def get_urls(pages=30):
     """Get urls for vnexpress categories. Each category may span hundreds of pages.    
@@ -32,17 +33,24 @@ class VnexpressSpider(scrapy.Spider):
     }
 
     start_urls = get_urls(pages=30)
-    ac = framler.AutoCrawlParser("requests")  
-
+    
     def parse(self, response):
         category = response._url
         for article in response.xpath('//article'):
+
             urlCrawl = article.xpath('div/a/@href').get()
+
+            page = requests.get(urlCrawl)
+
+            soup = BeautifulSoup(page.content, "html.parser")
+
+            job_elements = soup.find_all("article")
+
             yield {
                 'category': category,
                 'url': urlCrawl,
                 'title': article.xpath('div/a/@title').get(),
                 'text': article.xpath('p/a/text()').get(),
-                'content': ac.parse(urlCrawl) 
+                'content': ''.join([td.get_text() for job_element in job_elements for td in job_element.find_all("p", class_="Normal")])
             }
 
